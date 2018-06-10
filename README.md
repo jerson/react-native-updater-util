5e5a2eb0a26f3bd3922db953624d995b0297bfa9
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2018-06-09 22:57:28 -0500

    updated docs

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
 # react-native-updater-util
 
+method for open installer "apk" only **Android**
+
 ## Getting started
 
 `$ npm install react-native-updater-util --save`
@@ -11,13 +13,6 @@
 
 ### Manual installation
 
-
-#### iOS
-
-1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
-2. Go to `node_modules` ➜ `react-native-updater-util` and add `RNUpdaterUtil.xcodeproj`
-3. In XCode, in the project navigator, select your project. Add `libRNUpdaterUtil.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
-4. Run your project (`Cmd+R`)<
 
 #### Android
 
@@ -37,9 +32,8 @@
 
 ## Usage
 ```javascript
-import RNUpdaterUtil from 'react-native-updater-util';
+import UpdaterUtil from 'react-native-updater-util';
 
-// TODO: What to do with the module?
-RNUpdaterUtil;
+UpdaterUtil.openInstaller(path)
 ```
   
--- android/src/main/java/me/jerson/mobile/updater/RNUpdaterUtilModule.java
+++ android/src/main/java/me/jerson/mobile/updater/RNUpdaterUtilModule.java
@@ -18,57 +18,59 @@ import com.facebook.react.module.annotations.ReactModule;
 
 import java.io.File;
 import java.util.List;
- 
+
 public class RNUpdaterUtilModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNUpdaterUtilModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNUpdaterUtilModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNUpdaterUtil";
-  }
+    @Override
+    public String getName() {
+        return "RNUpdaterUtil";
+    }
 
-  @ReactMethod
-  public void openInstaller(String path, Promise promise) {
+    @ReactMethod
+    public void openInstaller(String path, Promise promise) {
 
-      if (reactContext == null) {
-          promise.reject("400", "error");
-          return;
-      }
+        if (reactContext == null) {
+            promise.reject("400", "error");
+            return;
+        }
 
-      try {
+        try {
 
-          if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-              Intent intent = new Intent(Intent.ACTION_VIEW);
-              Uri uri = FileProvider.getUriForFile(reactContext, reactContext.getApplicationContext().getPackageName() + ".provider", new File(path));
-              intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-              intent.setDataAndType(uri, "application/vnd.android.package-archive");
-              intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-              List<ResolveInfo> resInfoList = reactContext.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-              for (ResolveInfo resolveInfo : resInfoList) {
-                  String packageName = resolveInfo.activityInfo.packageName;
-                  reactContext.grantUriPermission(packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-              }
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = FileProvider.getUriForFile(reactContext,
+                        reactContext.getApplicationContext().getPackageName() + ".provider", new File(path));
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.setDataAndType(uri, "application/vnd.android.package-archive");
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                List<ResolveInfo> resInfoList = reactContext.getPackageManager().queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    reactContext.grantUriPermission(packageName, uri,
+                            Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
 
-              reactContext.startActivity(intent);
-          } else {
-              Intent intent = new Intent(Intent.ACTION_VIEW);
-              Uri uri = Uri.fromFile(new File(path));
-              intent.setDataAndType(uri, "application/vnd.android.package-archive");
-              intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-              reactContext.startActivity(intent);
-          }
+                reactContext.startActivity(intent);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.fromFile(new File(path));
+                intent.setDataAndType(uri, "application/vnd.android.package-archive");
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                reactContext.startActivity(intent);
+            }
 
-
-          promise.resolve(true);
-      } catch (Exception e) {
-          Log.w(TAG, e);
-          promise.reject(e);
-      }
-  }
+            promise.resolve(true);
+        } catch (Exception e) {
+            Log.w(TAG, e);
+            promise.reject(e);
+        }
+    }
 }
